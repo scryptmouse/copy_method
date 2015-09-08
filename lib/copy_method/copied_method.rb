4127848dcd00a4c9fddf4869974baaac4f7e7374
Alexa Grey <devel@mouse.vc>
2015-09-08 03:33:26 -0700

    Fix tests for rubinius; travis matrix
    RBX apparently has a secret 'origin' method on
    modules / classes.

--- lib/copy_method/copied_method.rb
+++ lib/copy_method/copied_method.rb
@@ -5,31 +5,48 @@ module CopyMethod
     # @return [Symbol]
     attr_reader :method_name
 
-    # @!attribute [r] origin
+    # @!attribute [r] original_class
     # @return [Symbol]
-    attr_reader :origin
+    attr_reader :original_class
 
     attr_reader :singleton
     alias singleton? singleton
 
     # @param [Symbol] method_name
-    # @param [Class] origin
+    # @param [Class] original_class
     # @param [Boolean] singleton
-    def initialize(origin, method_name, singleton: false)
-      @method_name  = method_name
-      @origin       = origin.to_s.to_sym
-      @singleton    = singleton
+    def initialize(original_class, method_name, singleton: false)
+      @method_name      = method_name
+      @original_class   = original_class.name.to_sym
+      @singleton        = singleton
 
+      add_const_missing!
+
+      super() if defined?(super)
+    end
+
+    def inspect
+      "CopyMethod::CopiedMethod(#{original_class}#{method_symbol}#{method_name})"
+    end
+
+    private
+    def quoted_method_name
+      method_name.to_s.inspect
+    end
+
+    def qualified_original_class
+      "::#{original_class}"
+    end
+
+    def add_const_missing!
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def const_missing(const_name)
           caller_method = caller_locations(1,1)[0].label
 
           super
         rescue NameError => e
-          origin = ::#{origin}
-
-          if caller_method == #{method_name.to_s.inspect} && origin.const_defined?(const_name)
-            origin.const_get const_name
+          if caller_method == #{quoted_method_name} && #{qualified_original_class}.const_defined?(const_name)
+            #{qualified_original_class}.const_get const_name
           else
             raise e
           end
@@ -37,11 +54,6 @@ module CopyMethod
       RUBY
     end
 
-    def inspect
-      "CopyMethod::CopiedMethod(#{origin}#{method_symbol}#{method_name})"
-    end
-
-    private
     def method_symbol
       singleton? ? '.' : '#'
     end
